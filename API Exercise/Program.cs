8b36ca0bfe28258bca0c4657e8af8004dbd4f106
Daniel Aguirre <uhyeay24@gmail.com>
2021-09-21 13:05:56 -0500

    exercise complete

---
+++ API Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace API_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(150, 40);
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("\nKanye: " + JObject.Parse(client.GetStringAsync("https://api.kanye.rest").Result)
+                    .GetValue("quote").ToString());
+
+                Console.WriteLine("\nRon: " + JArray.Parse(client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result)
+                    .ToString().Replace('[', ' ').Replace(']', ' ').Trim());
+            }
+        }
+    }
+}
